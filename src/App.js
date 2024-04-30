25c01a05f74b1783413cc846fb54acc2b8f634bd
Von Bock <vonjbock@gmail.com>
2024-04-29 22:00:19 -0400

    added creatures image folder and new Ram creature image. Altered animations and card content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './creatures/Creature_Ram.png';
 import './App.css';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
@@ -27,30 +27,44 @@ function App() {
         <header className="App-header">
           <Card sx={{ maxWidth: 345, backgroundColor: '#282c34' }} className="card-component" raised="true">
             <Box>
-          <CardMedia
-              component="img"
-              image={logo}
-              alt="Live from space album cover"
-              className='App-logo'
-            />
-            <CardContent>
-          <Button>
-            Test
-          </Button>
-          <Typography>
-            Edit <code>src/App.js</code> and save to reload.
-          </Typography>
-          <Link
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </Link>
-          </CardContent>
-          </Box>
+              <CardMedia
+                  component="img"
+                  image={logo}
+                  alt="Live from space album cover"
+                  className='App-logo'
+                />
+                <CardContent>
+                  <Typography> Ram </Typography>
+                  <Button>
+                    Feed
+                  </Button>
+                  <Button>
+                    Play
+                  </Button>
+                  <Typography>
+                    Make sure to play with and feed your creature!
+                  </Typography>
+                  
+              </CardContent>
+            </Box>
           </Card>
+          {/* <Box>
+            <Typography>
+
+
+
+              <Link
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+                Help?
+              </Link>
+            </Typography>
+          
+          </Box> */}
+          
         </header>
       </div>
     </ThemeProvider>
